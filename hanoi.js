d093653255ba3a430de6da31178f9017e8ea5c41
Martin-I23 <143592042+Martin-I23@users.noreply.github.com>
2023-09-02 16:22:52 +0300

    Create hanoi.js

---
+++ hanoi.js
@@ -0,0 +1,8 @@
+
+function openNav(){
+    document.getElementById("myside").style.width = "250px";
+    }
+    
+    function closeNav(){
+    document.getElementById("myside").style.width = "0px";
+    }
